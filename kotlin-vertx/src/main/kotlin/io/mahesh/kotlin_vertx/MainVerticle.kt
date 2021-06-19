d5805e437cfe1d01dd62153186bb19b693179689
mahesh <mail.mahesh.lk@gmail.com>
2021-06-19 21:10:20 +0800

    vertx-kotlin

--- kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/MainVerticle.kt
+++ kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/MainVerticle.kt
@@ -1,9 +1,32 @@
 package io.mahesh.kotlin_vertx
 
+import io.vertx.core.Handler
+import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
+import io.vertx.core.eventbus.Message
+import io.vertx.core.json.JsonObject
 import org.slf4j.LoggerFactory
 
 class MainVerticle {
   private val logger = LoggerFactory.getLogger("io.mahesh.kotlin_vertx.MainVerticle")
 
+  fun main(){
+
+    // nonclusterdeploy();
+    // Vertx.clusteredVertx();
+    //Vertx vertx = nonclusterdeployment();
+    Vertx.clusteredVertx(VertxOptions())
+      .onSuccess { vertx: Vertx ->
+        vertx.deployVerticle(CovidcasesCollector())
+        vertx.eventBus()
+          .consumer("who.portal",
+            Handler { message: Message<JsonObject> ->
+              logger.info(
+                "New Covid update....{}",
+                message.body().encodePrettily()
+              )
+            })
+      }.onFailure { fail: Throwable? -> logger.error("shit..crash") }
+  }
 
 }

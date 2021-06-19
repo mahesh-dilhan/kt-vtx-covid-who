7d249cdbbf00920bbf4d08fe26c38e0d1f07ac83
mahesh <mail.mahesh.lk@gmail.com>
2021-06-19 20:47:06 +0800

    vertx-kotlin

--- kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/CovidcasesCollector.kt
+++ kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/CovidcasesCollector.kt
@@ -3,10 +3,16 @@ package io.mahesh.kotlin_vertx
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 import org.slf4j.LoggerFactory
+import java.util.*
+
 
 class CovidcasesCollector : AbstractVerticle() {
   private val logger = LoggerFactory.getLogger("io.mahesh.kotlin_vertx.CovidcasesCollector")
   private val PORT = System.getProperty("HTTP_PORT", "8888").toIntOrNull() ?: 8888
+  private val countries = mutableListOf("USA", "SL", "IND", "PK", "AUS")
+  private val randomcountry = Random()
+  
+
 
   override fun start(startPromise: Promise<Void>) {
     vertx

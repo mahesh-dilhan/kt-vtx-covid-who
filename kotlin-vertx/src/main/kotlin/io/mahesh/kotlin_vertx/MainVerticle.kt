f940df381846e14c7d7c050b761aae6e57692a0b
mahesh <mail.mahesh.lk@gmail.com>
2021-06-19 21:08:00 +0800

    vertx-kotlin

--- kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/MainVerticle.kt
+++ kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/MainVerticle.kt
@@ -1,4 +1,9 @@
 package io.mahesh.kotlin_vertx
 
+import org.slf4j.LoggerFactory
+
 class MainVerticle {
+  private val logger = LoggerFactory.getLogger("io.mahesh.kotlin_vertx.MainVerticle")
+
+
 }

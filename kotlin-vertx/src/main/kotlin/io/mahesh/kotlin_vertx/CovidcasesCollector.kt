ed0da67b74e60477413a194b5dc06d1ca4692ede
mahesh <mail.mahesh.lk@gmail.com>
2021-06-19 20:22:33 +0800

    vertx-kotlin

---
+++ kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/CovidcasesCollector.kt
@@ -0,0 +1,25 @@
+package io.mahesh.kotlin_vertx
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Promise
+
+class CovidcasesCollector : AbstractVerticle() {
+
+  override fun start(startPromise: Promise<Void>) {
+    vertx
+      .createHttpServer()
+      .requestHandler { req ->
+        req.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello from Vert.x!")
+      }
+      .listen(8888) { http ->
+        if (http.succeeded()) {
+          startPromise.complete()
+          println("HTTP server started on port 8888")
+        } else {
+          startPromise.fail(http.cause());
+        }
+      }
+  }
+}

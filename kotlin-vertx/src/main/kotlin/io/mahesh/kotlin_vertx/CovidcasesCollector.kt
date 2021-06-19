9338c41e22117e4730ac7f76e21650d6c215a121
mahesh <mail.mahesh.lk@gmail.com>
2021-06-19 21:01:55 +0800

    vertx-kotlin

--- kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/CovidcasesCollector.kt
+++ kotlin-vertx/src/main/kotlin/io/mahesh/kotlin_vertx/CovidcasesCollector.kt
@@ -18,21 +18,7 @@ class CovidcasesCollector : AbstractVerticle() {
   private val data = mutableMapOf<String, Country>()
 
   override fun start(startPromise: Promise<Void>) {
-    vertx
-      .createHttpServer()
-      .requestHandler { req ->
-        req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!")
-      }
-      .listen(8888) { http ->
-        if (http.succeeded()) {
-          startPromise.complete()
-          println("HTTP server started on port 8888")
-        } else {
-          startPromise.fail(http.cause());
-        }
-      }
+    
   }
 
   fun list(routingContext: RoutingContext){
